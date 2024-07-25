e2f86a5a573cc3d1e930d4c9bdacdc3ad5a3ecfa
Kasidit Bualoeng <subdit@gmail.com>
2024-07-25 15:09:56 -0400

    create react drag and drop app

---
+++ drag-and-drop/src/App.jsx
@@ -0,0 +1,14 @@
+import NotesPage from './pages/NotesPage';
+
+function App() {
+  return (
+    <>
+      <div id='app'>
+        <h2>Hello from Vite</h2>
+        <NotesPage />
+      </div>
+    </>
+  );
+}
+
+export default App;
