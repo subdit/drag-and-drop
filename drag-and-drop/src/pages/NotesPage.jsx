4a9ddaaf8a1f07e48557c3f5b3aef5de3af2c3a4
Kasidit Bualoeng <subdit@gmail.com>
2024-07-30 19:00:13 -0400

    fixing bug on rendering database from the endpoint appwrithe.io

--- drag-and-drop/src/pages/NotesPage.jsx
+++ drag-and-drop/src/pages/NotesPage.jsx
@@ -1,7 +1,22 @@
-import { fakeData as notes } from '../assets/fakeData.js';
+import { useState, useEffect } from 'react';
+// import { fakeData as notes } from '../assets/fakeData.js';
+import { databases } from '../appwrite/config';
 import NoteCard from '../components/NoteCard';
 
 const NotesPage = () => {
+  const [notes, setNotes] = useState([]);
+  useEffect(() => {
+    init();
+  }, []);
+  // set empty array not to call more than once.
+  const init = async () => {
+    const response = await databases.listDocuments(
+      import.meta.env.VITE_DATABASE_ID,
+      import.meta.env.VITE_COLLECTION_NOTES_ID
+    );
+    console.log(response);
+  };
+
   return (
     <div>
       {notes.map(note => (
