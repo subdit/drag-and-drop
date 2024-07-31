965f5176db659f0213205951618c40cbed6252d6
Kasidit Bualoeng <subdit@gmail.com>
2024-07-31 09:46:30 -0400

    styling spinner and set loading state

--- drag-and-drop/src/context/NoteContext.jsx
+++ drag-and-drop/src/context/NoteContext.jsx
@@ -5,10 +5,34 @@ export const NoteContext = createContext();
 
 const NoteProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
-  const contextData = {};
+  const [notes, setNotes] = useState();
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  const init = async () => {
+    const reponse = await db.notes.list();
+    setNotes(reponse.documents);
+    setLoading(false);
+  };
+
+  const contextData = { notes, setNotes };
   return (
     <NoteContext.Provider value={contextData}>
-      {loading ? <Spinner /> : children}
+      {loading ? (
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100vh'
+          }}>
+          <Spinner size='100' />
+        </div>
+      ) : (
+        children
+      )}
     </NoteContext.Provider>
   );
 };
